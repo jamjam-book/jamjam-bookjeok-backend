6c6d89927bfe35e38aaaa2eb8c87f19976a2331e
hnjee <leehnjee@gmail.com>
2025-04-03 17:53:00 +0900

    feat: 프로젝트 초기 설정

---
+++ src/test/java/com/jamjam/bookjeok/BookjeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamjam.bookjeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookjeokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
