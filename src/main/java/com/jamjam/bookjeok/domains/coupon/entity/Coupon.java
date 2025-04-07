a2520d68d11738c96d9b5180df52de7e46e9f61b
hnjee <leehnjee@gmail.com>
2025-04-07 17:13:29 +0900

    #38 refector: entity 아이디 이름 수정 빠진거 추가

--- src/main/java/com/jamjam/bookjeok/domains/coupon/entity/Coupon.java
+++ src/main/java/com/jamjam/bookjeok/domains/coupon/entity/Coupon.java
@@ -17,7 +17,7 @@ public class Coupon {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "coupon_id")
-    private Long id;
+    private Long couponId;
 
     @Column(name = "coupon_name", nullable = false)
     private String couponName;
