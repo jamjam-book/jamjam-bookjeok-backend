c829fb6760d7694b18f68f8cd53616132c27020a
devyujinjeong <devyujinjeong887@gmail.com>
2025-04-10 21:41:41 +0900

    feat: 게시글 목록 요약 DTO 생성

---
+++ src/main/java/com/jamjam/bookjeok/domains/member/dto/PostSummaryDTO.java
@@ -0,0 +1,14 @@
+package com.jamjam.bookjeok.domains.member.dto;
+
+import lombok.*;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostSummaryDTO {
+
+    private String nickname;
+    private String title;
+
+}
