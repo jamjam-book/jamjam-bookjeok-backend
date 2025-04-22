9ba2377ab9e317d44756d354db57f9f18feaca13
devyujinjeong <devyujinjeong887@gmail.com>
2025-04-22 23:35:43 +0900

    refactor : Member 엔티티 패키지명 수정

--- be15-jamjam-bookjeok-msa/be15-2nd-jamjam-bookjeok/src/main/java/com/jamjam/bookjeok/domains/member/command/repository/MemberRepository.java
+++ be15-jamjam-bookjeok-msa/be15-2nd-jamjam-bookjeok/src/main/java/com/jamjam/bookjeok/domains/member/command/repository/MemberRepository.java
@@ -2,12 +2,12 @@ package com.jamjam.bookjeok.domains.member.command.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.jamjam.bookjeok.domains.member.entity.Member;
+import com.jamjam.bookjeok.domains.member.command.entity.Member;
 
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends JpaRepository<com.jamjam.bookjeok.domains.member.entity.Member, Long>{
+public interface MemberRepository extends JpaRepository<Member, Long>{
 
     Optional<Member> findByMemberId(String memberId);
 
