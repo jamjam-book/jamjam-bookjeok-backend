20960752f8311b97c18f718a543fa2198ea26af6
devyujinjeong <devyujinjeong887@gmail.com>
2025-05-02 11:04:42 +0900

    feat : 비밀번호 재설정 페이지 만들기 (#4)

--- src/features/member/router.js
+++ src/features/member/router.js
@@ -1,1 +1,12 @@
-export const memberRoutes = () => {};
+export const memberRoutes = [
+    {
+        path: '/password/reset-link',
+        name: 'PasswordResetRequest',
+        component: () => import('@/features/member/views/PasswordResetRequestView.vue')
+    },
+    {
+        path: '/password/reset',
+        name: 'PasswordReset',
+        component: () => import('@/features/member/views/PasswordResetView.vue')
+    }
+];
