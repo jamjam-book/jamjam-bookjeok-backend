d5a471d993c84a6ca9e457d459af23066ed91a7c
devyujinjeong <devyujinjeong887@gmail.com>
2025-05-04 23:03:53 +0900

    feat : 관리자 페이지 회원 목록 가져오는 페이지 및 공통 컴포넌트 생성 (#5)

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <script setup>
 
-import Footer from "@/components/layout/Footer.vue"
-import MyPageNav from "@/components/common/MyPageNav.vue";
-import AdminNav from "@/components/layout/AdminNav.vue";
+import Footer from "@/components/layout/Footer.vue";
 import Header from "@/components/layout/Header.vue";
+import { useRoute } from "vue-router";
+import {computed} from "vue";
+
+const route = useRoute();
+// 관리자 경로인지 확인하는 부분
+const isAdminRoute = computed(() => {
+    const isAdmin = route.path.startsWith('/admin');
+    return isAdmin;
+});
 </script>
 
 <template>
-    <Header/>
-    <div class="container">
-          <RouterView/>
-    <!--    <MyPageNav/>-->
-    <!--    <AdminNav/>-->
+    <!-- 관리자가 아니면 헤더 표시 -->
+    <Header v-if="!isAdminRoute" />
+
+    <!-- 관리자가 아니면 container 클래스 적용 -->
+    <div :class="{ container: !isAdminRoute }">
+        <RouterView />
     </div>
-    <Footer/>
+
+    <!-- 관리자가 아니면 푸터 표시 -->
+    <Footer v-if="!isAdminRoute" />
 </template>
 
 <style scoped>
