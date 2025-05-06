306b60af8a43f0d712a8fd8ed5ef2a87a183e3d2
devyujinjeong <devyujinjeong887@gmail.com>
2025-05-06 22:48:59 +0900

    test : 관리자와 관심 작가 controller 테스트 수정

--- be15-jamjam-bookjeok-msa/be15-2nd-jamjam-bookjeok/src/test/java/com/jamjam/bookjeok/domains/member/query/controller/InterestAuthorQueryControllerTest.java
+++ be15-jamjam-bookjeok-msa/be15-2nd-jamjam-bookjeok/src/test/java/com/jamjam/bookjeok/domains/member/query/controller/InterestAuthorQueryControllerTest.java
@@ -1,6 +1,5 @@
 package com.jamjam.bookjeok.domains.member.query.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,9 +33,8 @@ public class InterestAuthorQueryControllerTest {
                         .param("size", "2"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
-                .andExpect(jsonPath("$.data").isArray())
-                .andExpect(jsonPath("$.data[0].authorName").value("정세랑"))
-                .andExpect(jsonPath("$.data[0].bookList[0].bookName").value("지구에서 한아뿐"))
+                .andExpect(jsonPath("$.data.interestAuthorList[0].authorName").value("정세랑"))
+                .andExpect(jsonPath("$.data.interestAuthorList[0].bookList[0].bookName").value("지구에서 한아뿐"))
                 .andExpect(jsonPath("$.timestamp").exists())
                 .andDo(print());
     }
