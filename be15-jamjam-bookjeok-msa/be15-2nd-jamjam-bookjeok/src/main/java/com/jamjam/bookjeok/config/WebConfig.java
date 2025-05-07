6489abc03776cb26516f19540c0b5e8e414f9fec
Hyeon Young Jang <gusdud9124@gmail.com>
2025-05-07 21:14:33 +0900

    fix: 운영 체제에 맞는 이미지 경로 설정
    - 운영 체제에 따라 이미지 디렉토리 경로를 동적으로 설정하여, Windows와 macOS에서 모두 정상적으로 이미지 파일에 접근할 수 있도록 수정.
    - application.yml에서 이미지 경로를 설정하고, WebConfig에서 이를 활용하도록 변경.

--- be15-jamjam-bookjeok-msa/be15-2nd-jamjam-bookjeok/src/main/java/com/jamjam/bookjeok/config/WebConfig.java
+++ be15-jamjam-bookjeok-msa/be15-2nd-jamjam-bookjeok/src/main/java/com/jamjam/bookjeok/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.jamjam.bookjeok.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${image.os-image-dir}")
+    private String osaImageDirectory;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -20,6 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///C:/jamjam/images"); // 이미지 저장 절대경로
+                .addResourceLocations(osaImageDirectory);
     }
+
 }
