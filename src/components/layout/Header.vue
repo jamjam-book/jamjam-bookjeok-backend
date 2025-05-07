70bc006b6093ee3dc18f51921843904bfc7d8dc8
Hyeon Young Jang <gusdud9124@gmail.com>
2025-05-07 18:24:17 +0900

    feat: 장바구니에 있는 도서 정보 개수 만큼 장바구니 아이콘 우측 상단에 개수를 표시하는 기능 구현

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,16 +1,31 @@
 <script setup>
-import {onBeforeUnmount, onMounted, ref} from 'vue'
-import SearchBar from "@/components/common/SearchBar.vue";
+import {onMounted, ref} from 'vue'
+import axios from 'axios'
 
 const isLoggedIn = ref(false);
 const memberStatus = ref('MEMBER');
-const cartCount = ref(0);
-const isOpen = ref(false)
+const cartCount = ref(0);  // 장바구니 수량을 저장하는 상태 변수
 
-const toggleDropdown = () => {
-    isOpen.value = !isOpen.value
+// 장바구니 수량을 가져오는 API 호출
+const getCartCount = async () => {
+    try {
+        const memberId = 'user01';
+        const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/members/${memberId}/carts/count`);
+        console.log(response.data); // 확인용 로그
+
+        if (response.data && response.data.data) {
+            cartCount.value = response.data.data.cartCount;
+            console.log(cartCount.value)
+        }
+    } catch (error) {
+        console.error('장바구니 수량을 가져오는 데 실패했습니다:', error);
+    }
 }
 
+// 컴포넌트가 마운트될 때 장바구니 수량을 가져옴
+onMounted(() => {
+    getCartCount();
+})
 </script>
 
 <template>
@@ -22,7 +37,6 @@ const toggleDropdown = () => {
                         <img class="logo" src="../../assets/images/logo.png" alt="로고"/>
                     </RouterLink>
                     <div class="header-dropdown">
-                        <!-- icon으로 바꾸는게 더 좋을듯!-->
                         <button v-if="!isOpen" @click="toggleDropdown" class="header-dropdown-toggle">▶</button>
                         <button v-else @click="toggleDropdown" class="header-dropdown-toggle">▼</button>
                         <div v-if="isOpen" class="header-dropdown-menu">
@@ -36,7 +50,6 @@ const toggleDropdown = () => {
 
                 <nav class="header-right">
                     <div class="icon-group">
-                        <!-- 로그인 전 -->
                         <template v-if="!isLoggedIn">
                             <div class="auth-links">
                                 <RouterLink to="/login" class="auth-link">로그인</RouterLink>
@@ -44,17 +57,18 @@ const toggleDropdown = () => {
                             </div>
                             <div class="header-icons">
                                 <RouterLink to="/carts">
-                                    <img class="icon" src="../../assets/icons/cart.png" alt="장바구니"/>
+                                    <div class="cart-wrapper">
+                                        <img class="icon" src="../../assets/icons/cart.png" alt="장바구니"/>
+                                        <span v-if="cartCount > 0" class="cart-count">{{ cartCount }}</span>
+                                    </div>
                                 </RouterLink>
                                 <img class="icon" src="../../assets/icons/mypage.png" alt="마이페이지"/>
                             </div>
                         </template>
 
-                        <!-- 관리자가 로그인 한 경우 -->
                         <template v-else-if="memberStatus === 'ADMIN'">
                             <div class="auth-links">
                                 <RouterLink to="/admin" class="auth-link">관리자</RouterLink>
-                                <!-- 로그아웃 된 다음에 메인 페이지로 이동할 예정 -->
                                 <a href="/logout" class="auth-link logout">로그아웃</a>
                             </div>
                             <div class="header-icons">
@@ -63,24 +77,21 @@ const toggleDropdown = () => {
                             </div>
                         </template>
 
-                        <!-- 회원이 로그인 한 경우 -->
                         <template v-else-if="memberStatus === 'MEMBER'">
                             <div class="auth-links">
-                                <!-- 로그아웃 된 다음에 메인 페이지로 이동할 예정 -->
                                 <a href="/logout" class="auth-link logout">로그아웃</a>
                             </div>
                             <div class="header-icons">
                                 <img class="icon favorite" src="../../assets/icons/favorite.png" alt="관심 목록"/>
                                 <div class="cart-wrapper">
                                     <img class="icon cart" src="../../assets/icons/cart.png" alt="장바구니"/>
-                                    <span class="cart-count">{{ cartCount }}</span>
+                                    <span v-if="cartCount > 0" class="cart-count">{{ cartCount }}</span>
                                 </div>
                                 <img class="icon" src="../../assets/icons/mypage.png" alt="마이페이지"/>
                             </div>
                         </template>
                     </div>
                 </nav>
-
             </div>
         </div>
     </div>
