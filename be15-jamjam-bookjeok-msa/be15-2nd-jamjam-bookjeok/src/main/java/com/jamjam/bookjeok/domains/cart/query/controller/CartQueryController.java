370bc621ce8cf6777fa24dd69e14b4a96b0d71e7
Hyeon Young Jang <gusdud9124@gmail.com>
2025-05-07 14:32:00 +0900

    feat: 회원의 장바구니 목록에 있는 도서 개수를 조회하는 기능 구현

--- be15-jamjam-bookjeok-msa/be15-2nd-jamjam-bookjeok/src/main/java/com/jamjam/bookjeok/domains/cart/query/controller/CartQueryController.java
+++ be15-jamjam-bookjeok-msa/be15-2nd-jamjam-bookjeok/src/main/java/com/jamjam/bookjeok/domains/cart/query/controller/CartQueryController.java
@@ -2,6 +2,7 @@ package com.jamjam.bookjeok.domains.cart.query.controller;
 
 import com.jamjam.bookjeok.common.dto.ApiResponse;
 import com.jamjam.bookjeok.domains.cart.query.dto.response.CartBookListResponse;
+import com.jamjam.bookjeok.domains.cart.query.dto.response.CartCountResponse;
 import com.jamjam.bookjeok.domains.cart.query.service.CartQueryService;
 import com.jamjam.bookjeok.domains.member.command.dto.response.MemberDetailResponse;
 import com.jamjam.bookjeok.domains.member.query.service.MemberQueryService;
@@ -30,6 +31,20 @@ public class CartQueryController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiResponse.success(cartBookListResponse));
+    }
+
+    @GetMapping("/members/{memberId}/carts/count")
+    public ResponseEntity<ApiResponse<CartCountResponse>> getCartCount(
+        @PathVariable(value = "memberId") String memberId
+    ) {
+        MemberDetailResponse memberDetail = memberQueryService.getMemberDetail(memberId);
+        Long memberUid = memberDetail.getMember().getMemberUid();
+
+        CartCountResponse cartCountResponse = cartQueryService.getCartCount(memberUid);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success(cartCountResponse));
     }
 
 }
