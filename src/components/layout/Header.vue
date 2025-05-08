48fd67fe29f15fd99e0b5c44dea128b7983bf6b8
Jayboo816 <jayboo8160@gmail.com>
2025-05-08 10:48:16 +0900

    feat: 로그인 성공 시 헤더부분 변경 기능, 새로고침해도 로그인 상태 유지

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,26 +1,37 @@
-
 <script setup>
-import {ref, computed, onMounted} from 'vue'
-import {useAuthStore} from '@/stores/auth'
-import {storeToRefs} from 'pinia'
-import {useRouter} from 'vue-router'
-import SearchBar from "@/components/common/SearchBar.vue"
+import {onMounted, ref} from 'vue'
+import axios from 'axios'
+import SearchBar from "@/components/common/SearchBar.vue";
 
-const authStore = useAuthStore()
-const {isAuthenticated, userRole} = storeToRefs(authStore)
-const router = useRouter()
-
-const cartCount = ref(0)
+const isLoggedIn = ref(false);
+const memberStatus = ref('MEMBER');
+const cartCount = ref(0);  // 장바구니 수량을 저장하는 상태 변수
 const isOpen = ref(false)
 
 const toggleDropdown = () => {
     isOpen.value = !isOpen.value
 }
 
-const logoutHandler = () => {
-    authStore.clearAuth()
-    router.push('/')
+// 장바구니 수량을 가져오는 API 호출
+const getCartCount = async () => {
+    try {
+        const memberId = 'user01';
+        const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/members/${memberId}/carts/count`);
+        console.log(response.data); // 확인용 로그
+
+        if (response.data && response.data.data) {
+            cartCount.value = response.data.data.cartCount;
+            console.log(cartCount.value)
+        }
+    } catch (error) {
+        console.error('장바구니 수량을 가져오는 데 실패했습니다:', error);
+    }
 }
+
+// 컴포넌트가 마운트될 때 장바구니 수량을 가져옴
+onMounted(() => {
+    getCartCount();
+})
 </script>
 
 <template>
@@ -45,58 +56,44 @@ const logoutHandler = () => {
 
                 <nav class="header-right">
                     <div class="icon-group">
-                        <!-- 로그인 전 -->
-                        <template v-if="!isAuthenticated">
+                        <template v-if="!isLoggedIn">
                             <div class="auth-links">
                                 <RouterLink to="/login" class="auth-link">로그인</RouterLink>
                                 <RouterLink to="/signup" class="auth-link">회원가입</RouterLink>
                             </div>
                             <div class="header-icons">
                                 <RouterLink to="/carts">
-                                    <img class="icon" src="../../assets/icons/cart.png" alt="장바구니"/>
+                                    <div class="cart-wrapper">
+                                        <img class="icon" src="../../assets/icons/cart.png" alt="장바구니"/>
+                                        <span v-if="cartCount > 0" class="cart-count">{{ cartCount }}</span>
+                                    </div>
                                 </RouterLink>
-                                <RouterLink to="/profile">
-                                    <img class="icon" src="../../assets/icons/mypage.png" alt="마이페이지"/>
-                                </RouterLink>
+                                <img class="icon" src="../../assets/icons/mypage.png" alt="마이페이지"/>
                             </div>
                         </template>
 
-                        <!-- 관리자 로그인 -->
-                        <template v-else-if="userRole === 'ADMIN'">
+                        <template v-else-if="memberStatus === 'ADMIN'">
                             <div class="auth-links">
                                 <RouterLink to="/admin" class="auth-link">관리자</RouterLink>
-                                <button @click="logoutHandler" class="auth-link logout"
-                                        style="all: unset; cursor: pointer;">
-                                    로그아웃
-                                </button>
+                                <a href="/logout" class="auth-link logout">로그아웃</a>
                             </div>
                             <div class="header-icons">
                                 <img class="icon" src="../../assets/icons/cart.png" alt="장바구니"/>
-                                <RouterLink to="/profile">
-                                    <img class="icon" src="../../assets/icons/mypage.png" alt="마이페이지"/>
-                                </RouterLink>
+                                <img class="icon" src="../../assets/icons/mypage.png" alt="마이페이지"/>
                             </div>
                         </template>
 
-                        <!-- 회원 로그인 -->
-                        <template v-else>
+                        <template v-else-if="memberStatus === 'MEMBER'">
                             <div class="auth-links">
-                                <button @click="logoutHandler" class="auth-link logout"
-                                        style="all: unset; cursor: pointer;">
-                                    로그아웃
-                                </button>
+                                <a href="/logout" class="auth-link logout">로그아웃</a>
                             </div>
                             <div class="header-icons">
                                 <img class="icon favorite" src="../../assets/icons/favorite.png" alt="관심 목록"/>
                                 <div class="cart-wrapper">
-                                    <RouterLink to="/carts">
-                                        <img class="icon cart" src="../../assets/icons/cart.png" alt="장바구니"/>
-                                        <span class="cart-count">{{ cartCount }}</span>
-                                    </RouterLink>
+                                    <img class="icon cart" src="../../assets/icons/cart.png" alt="장바구니"/>
+                                    <span v-if="cartCount > 0" class="cart-count">{{ cartCount }}</span>
                                 </div>
-                                <RouterLink to="/profile">
-                                    <img class="icon" src="../../assets/icons/mypage.png" alt="마이페이지"/>
-                                </RouterLink>
+                                <img class="icon" src="../../assets/icons/mypage.png" alt="마이페이지"/>
                             </div>
                         </template>
                     </div>
